7a17bc4e296f2c26c7a553f29380178991c355b2
FRANCISCO KAMMERS LHEMANN <francisco.lhemann@alunos.sc.senac.br>
2024-10-30 17:07:18 -0300

    feat: add commection end all files

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,31 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> generes = new List<Genre>
+            {
+                new Genre
+                {
+                    Id = 1,
+                    Name = "Romance"
+                },
+                new Genre
+                {
+                    Id = 2,
+                    Name = "Ficção Cientifica"
+                },
+                new Genre
+                {
+                    Id= 3,
+                    Name = "Comedia"
+                }
+            };
+            return View(generes);
+        }
+    }
+}
