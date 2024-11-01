281483a61a21e6c7071ef03a708c3ee9bf49b711
francisco lhemann <francisco.lhemann@gmail.com>
2024-11-01 16:52:32 -0300

    feat: add connection in database

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -1,31 +1,25 @@
-﻿using Bookstore.Models;
+﻿using Bookstore.Data;
+using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookstore.Controllers
 {
     public class GenresController : Controller
     {
+        private readonly  BookstoreContext _context;
+
+        public GenresController(BookstoreContext context)
+        {
+            _context = context;
+        }
+
+
+
         public IActionResult Index()
         {
-            List<Genre> generes = new List<Genre>
-            {
-                new Genre
-                {
-                    Id = 1,
-                    Name = "Romance"
-                },
-                new Genre
-                {
-                    Id = 2,
-                    Name = "Ficção Cientifica"
-                },
-                new Genre
-                {
-                    Id= 3,
-                    Name = "Comedia"
-                }
-            };
-            return View(generes);
+                    
+           
+            return View(_context.Genres.ToList());
         }
     }
 }
